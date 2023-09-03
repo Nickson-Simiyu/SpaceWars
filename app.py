34e67b92802742a740deda7c3ee01572592129b4
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-09-03 11:29:17 +0300

    SpaceWars

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+import subprocess
+
+app = Flask(__name__, template_folder='templates')  # Use 'templates' as the template folder
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/new-game')
+def new_game():
+    print("Starting the game...")
+    subprocess.Popen(['C:\python311\python.exe', 'pygame_app.py'])
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
